213140316bbcc03ed415a4318840aad750ee279e
Aster Haven (@therebelrobot) <asterhaven@pm.me>
2025-03-12 19:05:19 -0400

    upgraded tailwind to v4

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { SITE_TITLE } from "../config";
 
 const Header = ({ title }: { title?: string }) => {
   return (
-    <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
+    <div className="sticky top-0 z-40 w-full backdrop-blur-sm flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
       <header className="max-w-8xl mx-auto">
         <div className="flex flex-row justify-between py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
           <a href="/">
