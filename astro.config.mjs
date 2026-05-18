b9863be4b2e4b6f181abfe248abd323dae463470
Aster Haven (@therebelrobot) <asterhaven@pm.me>
2026-05-18 13:23:49 -0400

    still fixing auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,9 +38,9 @@ const config = {
   },
   vite: {
     define: {
-      'process.env.KEYSTATIC_GITHUB_CLIENT_ID': 'process.env.KEYSTATIC_GITHUB_CLIENT_ID',
-      'process.env.KEYSTATIC_GITHUB_CLIENT_SECRET': 'process.env.KEYSTATIC_GITHUB_CLIENT_SECRET',
-      'process.env.KEYSTATIC_SECRET': 'process.env.KEYSTATIC_SECRET',
+      'process.env.KEYSTATIC_GITHUB_CLIENT_ID': JSON.stringify(process.env.KEYSTATIC_GITHUB_CLIENT_ID || ''),
+      'process.env.KEYSTATIC_GITHUB_CLIENT_SECRET': JSON.stringify(process.env.KEYSTATIC_GITHUB_CLIENT_SECRET || ''),
+      'process.env.KEYSTATIC_SECRET': JSON.stringify(process.env.KEYSTATIC_SECRET || ''),
     },
     optimizeDeps: {
       include: ["@mdx-js/react"],
